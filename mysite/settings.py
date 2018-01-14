29c669ba500e5c43a1988f252f71f6909224fbbf
GoodOldTrimTab <squaredroot@protonmail.com>
2018-01-14 10:16:56 -0600

    My Django Girls app, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+8pp+*^nky*0m4ire1r=+3cqfuan1j1mmyl6ba1+io9)**zf6o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'djangoblogtest.pythonanywhere.com']
 
 
 # Application definition
